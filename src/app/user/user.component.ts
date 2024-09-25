65e24d3bfe4c711d0d2f84efd22a02dac52cd9cd
VoinaRares <voinarares14@gmail.com>
2024-09-25 16:06:43 +0300

    Lesson 2, added Inputs, Outputs and Emitter for User class

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  @Input({required:true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return "assets/users/" + this.avatar;
+  }
+
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
 }
