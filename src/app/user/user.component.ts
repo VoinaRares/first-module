2fa4f5c5467465f609430d2de4a388a89a8227c9
VoinaRares <voinarares14@gmail.com>
2024-10-07 14:55:47 +0300

    Switched App to being mased off Modules(older Angular model)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 // type User = {
 //   id: string;
@@ -12,10 +11,8 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent]
 })
 
 export class UserComponent {
